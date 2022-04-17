831a12d4c2b48245d7375d5e3b97bebf4470ad11
daluozha <561890199@qq.com>
2022-04-18 04:19:43 +0800

    update .gitignore

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Github Pages Tempate" />
+      <HelloWorld msg="Github Pages Template" />
     </div>
   </header>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -18,7 +18,7 @@ defineProps<{
 <style scoped>
 h1 {
   font-weight: 500;
-  font-size: 2.6rem;
+  font-size: 2rem;
   top: -10px;
 }
 
